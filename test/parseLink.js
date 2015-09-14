9c311829abcd1ba6f25e41fda4d69f4a3a42d30d
Brian Stoner <brian@duckduckgo.com>
2015-09-14 15:21:07 -0400

    make DDG.parse_link strip leading punctuation

--- test/parseLink.js
+++ test/parseLink.js
@@ -1,42 +1,19 @@
 describe('DDG.parse_link', function() {
 
-    it('should retreive matching links from different strings', function() {
-        var link1 = DDG.parse_link('foo <a href="http://foobar.com">bars</a>','url'),
-            link2 = DDG.parse_link("<a href='http://foobar.com'>foo</a> bars","url");
+    var tests = [
+        ['foo <a href="http://foobar.com">bars</a>', 'url', 'http://foobar.com/'],
+        ['<a href="http://foobar.com">foo</a> bars', 'url', 'http://foobar.com/'],
+        ['foo <a href="http://foo.bar">foobar</a> bar <a href="#">bash</a>', 'text', 'foobar'],
+        ['<b>foobar</b>', 'rest', 'foobar'],
+        ['foo <a href="http://foobar.com">bars</a>', 'text', 'bars'],
+        ['<a href="http://foobar.com">bars</a>foo<a href="http://foobar.com">bars</a>', 'rest', 'foobars'],
+        ['<a href="http://foobar.com">blah</a> - something', 'rest', 'something']
+    ];
 
-        expect(link1).toEqual(link2);
-    });
-
-    it('should extract text from first link within a string', function() {
-        var text1 = "foobar",
-            string = 'foo <a href="http://foo.bar">'+text1+'</a> bar <a href="#">bash</a>',
-            text2 = DDG.parse_link(string,"text");
-
-        expect(text1).toEqual(text2);
-    });
-
-    it('should fail gracefully and strip html if passed a string without a link', function() {
-        var string = "foobar";
-        
-        expect(string).toEqual(DDG.parse_link('<b>'+string+'</b>',"rest"));
-    });
-
-    it('should return different values when requesting different parts of the string', function() {
-        var string = 'foo <a href="http://foobar.com">bars</a>',
-            text1 = DDG.parse_link(string,'url'),
-            text2 = DDG.parse_link(string,'text');
-
-        expect(text1).not.toEqual(text2);
-    });
-    
-    it('should strip the first link and return only the text from the second link along with the un-wrapped text', function() {
-        var linkText = 'bars',
-            link = '<a href="http://foobar.com">'+linkText+'</a>',
-            string1 = 'foo',
-            string2 = link+string1+link,
-            parsed = DDG.parse_link(string2,'rest');
-
-        expect(parsed).toEqual(string1+linkText);
+    tests.forEach(function(test) {
+        it('should parse the string correctly', function() {
+            expect(DDG.parse_link(test[0], test[1])).toEqual(test[2]);
+        });
     });
 
 });
--- util.js
+++ util.js
@@ -441,6 +441,12 @@
             // remove the first link, return the remaining *text*
             $firstLink.remove();
             result = $tmp_p.text();
+            // trim + strip any leading punctuation:
+            result = result.trim();
+            if (result[0] && result[0].match(/\-|\:|\;/)) {
+                result = result.substr(1, result.length);
+            }
+            result = result.trim();
         } else if (wanted === "url") {
             result = $firstLink[0].href;
         }
